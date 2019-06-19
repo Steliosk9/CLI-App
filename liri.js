44262c1d3f603d8b14227791caf60578afa321a7
steliosk9 <steliosk9@gmail.com>
2019-06-19 06:41:36 -0500

    npm install request

--- liri.js
+++ liri.js
@@ -67,119 +67,119 @@ function ConcertInfo(parameter) {
 }
 
 //======================================== FUNCTION FOR MOVIE INFO & ONDB API =============================================
-function MovieInfo(parameter){
+function MovieInfo(parameter) {
 
-    if(parameter === undefined){
+    if (parameter === undefined) {
         inputParameter === "Mr Nobody"
         console.log("***************");
         fs.appendFileSync("log.txt", "-----------------------\n");
         console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
-        fs.appendFileSync("log.txt", "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/" +"\n");
+        fs.appendFileSync("log.txt", "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/" + "\n");
         console.log("It's on Netflix!");
         fs.appendFileSync("log.txt", "It's on Netflix!\n");
     }
 
-// Request with AXIOS
+    // Request with AXIOS
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&plot=full&apikey=46375a92";
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&plot=full&apikey=46375a92";
 
 
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
 
-axios.get(queryUrl).then(
-  function(response) {
-    console.log("Release Year: " + response.data.Title);
-    fs.appendFileSync("log.txt", "Title:" + response.data.Title + "\n");
-    console.log("Release Year: " + response.data.Year);
-    fs.appendFileSync("log.txt", "Year:" + response.data.Year + "\n");
-    console.log("Release Year: " + response.data.imdbRating);
-    fs.appendFileSync("log.txt", "imbd:" + response.data.imdbRating + "\n");
-    console.log("Release Year: " + response.data.Ratings[1]);
-    fs.appendFileSync("log.txt", "Rotten Tomatoes:" + response.data.Ratings[1] + "\n");
-    console.log("Release Year: " + response.data.Country);
-    fs.appendFileSync("log.txt", "country:" + response.data.Country + "\n");
-    console.log("Release Year: " + response.data.Language);    
-    fs.appendFileSync("log.txt", "Language:" + response.data.Language + "\n");
-    console.log("Release Year: " + response.data.Plot);
-    fs.appendFileSync("log.txt", "Plot:" + response.data.Plot + "\n");
-    console.log("Release Year: " + response.data.Actors);
-    fs.appendFileSync("log.txt", "Actors:" + response.data.Actors + "\n");
-  })
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log("Release Year: " + response.data.Title);
+            fs.appendFileSync("log.txt", "Title:" + response.data.Title + "\n");
+            console.log("Release Year: " + response.data.Year);
+            fs.appendFileSync("log.txt", "Year:" + response.data.Year + "\n");
+            console.log("Release Year: " + response.data.imdbRating);
+            fs.appendFileSync("log.txt", "imbd:" + response.data.imdbRating + "\n");
+            console.log("Release Year: " + response.data.Ratings[1]);
+            fs.appendFileSync("log.txt", "Rotten Tomatoes:" + response.data.Ratings[1] + "\n");
+            console.log("Release Year: " + response.data.Country);
+            fs.appendFileSync("log.txt", "country:" + response.data.Country + "\n");
+            console.log("Release Year: " + response.data.Language);
+            fs.appendFileSync("log.txt", "Language:" + response.data.Language + "\n");
+            console.log("Release Year: " + response.data.Plot);
+            fs.appendFileSync("log.txt", "Plot:" + response.data.Plot + "\n");
+            console.log("Release Year: " + response.data.Actors);
+            fs.appendFileSync("log.txt", "Actors:" + response.data.Actors + "\n");
+        })
 
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-    
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 
 };
 
- //=============================function for music (Spotify)===============================================//
+//=============================function for music (Spotify)===============================================//
 
-function Songinfo(parameter){
-  if(parameter === undefined){
-      inputParameter = "the Sign";  // Default
-  }
-
-  spotify.search(
-    {
-        type: "track",
-        query: inputParameter
-    },
-    function (err, data) {
-        if (err) {
-            console.log("Error occurred: " + err);
-
-            return;
-        }
- 
-         var songs = data.tracks.item;
-
-
-         for (var i = 0; i < songs.length; i++){
-            console.log("************Song Info*********");
-            fs.appendFileSync("log.txt", "**********SONG INFO*********\n");
-            console.log(i);
-            fs.appendFileSync("log.txt", i +"\n");
-            console.log("Song name: " + songs[i].name);
-            fs.appendFileSync("log.txt", "song name: " + songs[i].name +"\n");
-            console.log("Preview song: " + songs[i].preview_url);
-            fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url +"\n");
-            console.log("Album: " + songs[i].album.name);
-            fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
-            console.log("Artist(s): " + songs[i].artists[0].name);
-            fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
-            console.log("*****************************");  
-            fs.appendFileSync("log.txt", "*****************************\n")
-
-         }
-
+function Songinfo(parameter) {
+    if (parameter === undefined) {
+        inputParameter = "the Sign";  // Default
     }
 
-    //function for reading the random.txt file  
-    function Info(){
-        fs.readFile('random.txt', 'utf8', function(err, data){
-            if (err){ 
-                return console.log(err);
+    spotify.search(
+        {
+            type: "track",
+            query: inputParameter
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+
+                return;
             }
-            var dataArr = data.split(',');
-            UserInputs(dataArr[0], dataArr[1]);
-        });
-    }
-    
+
+            var songs = data.tracks.item;
+
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log("************Song Info*********");
+                fs.appendFileSync("log.txt", "**********SONG INFO*********\n");
+                console.log(i);
+                fs.appendFileSync("log.txt", i + "\n");
+                console.log("Song name: " + songs[i].name);
+                fs.appendFileSync("log.txt", "song name: " + songs[i].name + "\n");
+                console.log("Preview song: " + songs[i].preview_url);
+                fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url + "\n");
+                console.log("Album: " + songs[i].album.name);
+                fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
+                console.log("Artist(s): " + songs[i].artists[0].name);
+                fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
+                console.log("*****************************");
+                fs.appendFileSync("log.txt", "*****************************\n")
+
+            }
+        }
+    );
+};
+
+//function for reading the random.txt file  
+function Info() {
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        var dataArr = data.split(',');
+        UserInputs(dataArr[0], dataArr[1]);
+    });
+}
