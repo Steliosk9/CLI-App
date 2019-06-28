202cc7af5da8c23caed001d5ec66122eddf477d5
steliosk9 <steliosk9@gmail.com>
2019-06-28 13:22:38 -0500

    refactoring the spotify api without a promise

--- liri.js
+++ liri.js
@@ -14,7 +14,7 @@ var axios = require("axios");
 var option = process.argv[2];
 var parameter = process.argv[3];
 
-
+userInputs(option, parameter);
 
 // We will then create a switch-case statement (if-else would also work).
 // The switch-case will direct which function gets run.
@@ -143,43 +143,37 @@ function MovieInfo(parameter) {
 
 //=============================function for music (Spotify)===============================================//
 
-function Songinfo(parameter) {
-    if (parameter === undefined) {
-        inputParameter = "the Sign";  // Default
-    }
 
-    spotify.search(
-        {
-            type: "track",
-            query: inputParameter
-        },
-        function (err, data) {
-            if (err) {
-                console.log("Error occurred: " + err);
+ function SongInfo(parameter){
 
-                return;
-            }
+    spotify.search({ type: 'track', query: parameter }, function(err, data) {
+        if (err) {
+          console.log('Error occurred: ' + err);
 
-            var songs = data.tracks.item;
-
-
-            for (var i = 0; i < songs.length; i++) {
-                console.log("************Song Info*********");
-                fs.appendFileSync("log.txt", "**********SONG INFO*********\n");
-                console.log(i);
-                fs.appendFileSync("log.txt", i + "\n");
-                console.log("Song name: " + songs[i].name);
-                fs.appendFileSync("log.txt", "song name: " + songs[i].name + "\n");
-                console.log("Preview song: " + songs[i].preview_url);
-                fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url + "\n");
-                console.log("Album: " + songs[i].album.name);
-                fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
-                console.log("Artist(s): " + songs[i].artists[0].name);
-                fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
-                console.log("*****************************");
-                fs.appendFileSync("log.txt", "*****************************\n")
-
-            }
+          return;
         }
-    );
-};
+       
+  
+      
+
+      var songs = data.tracks.items;
+
+                  for (var i = 0; i < songs.length; i++) {
+                      console.log("************Song Info*********");
+                      fs.appendFileSync("log.txt", "**********SONG INFO*********\n");
+                      console.log(i);
+                      fs.appendFileSync("log.txt", i + "\n");
+                      console.log("Song name: " + songs[i].name);
+                      fs.appendFileSync("log.txt", "song name: " + songs[i].name + "\n");
+                      console.log("Preview song: " + songs[i].preview_url);
+                      fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url + "\n");
+                      console.log("Album: " + songs[i].album.name);
+                      fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
+                      console.log("Artist(s): " + songs[i].artists[0].name);
+                      fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
+                      console.log("*****************************");
+                      fs.appendFileSync("log.txt", "*****************************\n")
+      
+                  }
+                });
+              }
